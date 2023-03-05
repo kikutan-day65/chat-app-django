0d64e399ee0b8604f5db1b1c7a4459c749ea153c
Haruto Mori <baystarsman@outlook.jp>
2023-03-05 19:45:18 +0100

    create a super user

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Room, Message
 
 # Register your models here.
+
+# need to register database models to admin page
+admin.site.register(Room)
+admin.site.register(Message)
