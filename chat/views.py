a358323b2c9fdba6f726f65b4d9a804af3f19346
Haruto Mori <baystarsman@outlook.jp>
2023-03-06 19:39:14 +0100

    fix bug

--- chat/views.py
+++ chat/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import Room, Message
+from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
-
 def home(request):
     return render(request, 'home.html')
 
-def room(request, room): # to collect the path of room, "room" is takens as argument here
+def room(request, room):
     username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
-
     return render(request, 'room.html', {
         'username': username,
         'room': room,
-        'room_details': room_details,
+        'room_details': room_details
     })
 
 def checkview(request):
@@ -22,11 +20,11 @@ def checkview(request):
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
-        return redirect('/' + room + '/?username=' + username)
+        return redirect('/'+room+'/?username='+username)
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return redirect('/' + room + '/?username=' + username)
+        return redirect('/'+room+'/?username='+username)
 
 def send(request):
     message = request.POST['message']
@@ -35,11 +33,10 @@ def send(request):
 
     new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
-    return HttpResponse('Message sent sucessfully')
+    return HttpResponse('Message sent successfully')
 
 def getMessages(request, room):
-    room_deatils = Room.object.get(name=room)
+    room_details = Room.objects.get(name=room)
 
-    messages = Message.objects.filter(room=room_deatils.id)
-
-    return JsonResponse({"messages": list(messages.values())})
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
